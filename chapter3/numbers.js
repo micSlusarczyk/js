8860cc87693bb975ac6577d98ce7b881efcbbddf
micSlusarczyk <michal199508@gmail.com>
2022-10-11 21:19:17 +0200

    basic arithmetical expressions

---
+++ chapter3/numbers.js
@@ -0,0 +1,45 @@
+console.log("Potęgowanie => Math.pow(2,53); ==>", Math.pow(2, 53));
+console.log(
+    "Zaokrąglenie do najbliższej liczby całkowitej => Math.round(.6); ==>",
+    Math.round(0.6)
+);
+console.log("Zaokrąglenie w góre => Math.ceil(.6); ==>", Math.ceil(0.6));
+console.log("Zaokrąglenie w dół => Math.floor(.6); ==>", Math.floor(0.6));
+console.log("Wartość bezwzględna => Math.abs(-5) ==>", Math.abs(-5));
+console.log(
+    "Wybranie największej wartości => Math.max(5,10,15) ==>",
+    Math.max(5, 10, 15)
+);
+console.log(
+    "Wybieranie najmniejszej wartości => Math.min(5,10,15) ==>",
+    Math.min(5, 10, 15)
+);
+console.log(
+    "Liczba pseudolosowa z zakresu 0 <= x < 1 => Math.random() ==>",
+    Math.random()
+);
+console.log(
+    "Liczba pseudolosowa z zakresu 0 <= x < 1 => Math.random().toFixed(2) z ilością miejsc po przecinku ==>",
+    Math.random().toFixed(2)
+);
+console.log(
+    "Liczba PI => Math.PI.toFixed(22) zaokrąglona do dwóch miejsc po przecinku ==>",
+    Math.PI.toFixed(22)
+);
+console.log("e: podstawa logarytmu naturalnego => Math.E ==>", Math.E);
+console.log("pierwiastek kwadratowy => Math.sqrt(3) ==>", Math.sqrt(3));
+console.log(
+    "Pierwiastek sześcienny => Math.pow(3, 1/3) ==>",
+    Math.pow(3, 1 / 3)
+);
+console.log("Funkcja trygonometryczna sinus => Math.sin(10) ==>", Math.sin(10));
+console.log("Logarytm naturalny z 10 => Math.log(10) ==>", Math.log(10));
+console.log(
+    "Logarytm liczby 100 przy podstawie 10 => Math.log(100)/Math.LN10 ==>",
+    Math.log(100) / Math.LN10
+);
+console.log(
+    "Logarytm liczby 512 przy podstawie 2 => Math.log(512)/Math.LN2 ==>",
+    Math.log(512) / Math.LN2
+);
+console.log("Sześcian stałej e => Math.exp(3) ==>", Math.exp(3));
